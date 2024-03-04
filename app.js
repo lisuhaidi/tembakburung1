9a3ad40057c484db3024131beb790039da08d1fa
Juli Suhaidi <157661842+lisuhaidi@users.noreply.github.com>
2024-03-04 02:04:05 +0000

    buat log agar nama muncul

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.post("/", async (req, res) => {
     });
   } else {
     await Score.insertOne({ nama: req.body.nama, score: "0" });
+    console.log(req.body.nama);
     res.render("player", {
       layout: "layouts/main-layouts",
       title: "Home",
